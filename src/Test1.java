61d298a2d0ebdb4d8e456814be94838274613f8c
anil7017 <anilkumarsahoo17@gmail.com>
2018-07-27 00:15:48 +0530

    hi

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+import java.util.Base64;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte[] bytesEncoded = Base64.getEncoder().
+
+	}
+
+}
